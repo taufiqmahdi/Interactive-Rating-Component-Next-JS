3fd5b24d5dd9cd10b1a05165768723de40be03a0
taufiqmahdi <57888672+taufiqmahdi@users.noreply.github.com>
2022-06-29 15:28:24 +0700

    Added logic for active button states, added a way to submit the form(rating), and added a local server api built in by Next to upload the form of rating

--- pages/main.js
+++ pages/main.js
@@ -1,84 +1,179 @@
-import React, { useState } from 'react'
-import Head from 'next/head'
-import { Box, Flex, Text, Button, ButtonGroup, useControllableProp, useControllableState } from "@chakra-ui/react"
+import React, { useState } from "react";
+import Head from "next/head";
+import {
+  Box,
+  Flex,
+  Text,
+  Button,
+  ButtonGroup,
+  useControllableProp,
+  useControllableState,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  RadioGroup,
+  Stack,
+} from "@chakra-ui/react";
 
 function About() {
+  //   const bgInactive = 'hsl(216, 12%, 54%)';
+  //   const bgActive = 'red';
 
-    const bgInactive = 'hsl(216, 12%, 54%)'
-    const bgActive = 'red'
+  const [isActive, setIsActive] = React.useState(null);
+  const feedbackScore = [1, 2, 3, 4, 5];
+  const chooseFeedback = (chooseIndex) => {
+    if (isActive === chooseIndex) setIsActive(null);
+    else setIsActive(chooseIndex);
+  };
+  const handleClick = () => {
+    if (isActive == bgInactive) {
+      setIsActive(bgActive);
+    } else {
+      setIsActive(bgInactive);
+    }
+  };
 
-    const [isActive, setIsActive] = React.useState(bgInactive) //useControllableState(bg='hsl(216, 12%, 54%)')
-
-    const handleClick = () => {
-        // 👇️ toggle
-        // setIsActive(current => !current);
-
-        if (isActive == bgInactive) {
-            setIsActive(bgActive);
-            console.log('if')
-        } else {
-            setIsActive(bgInactive);
-            console.log('uf')
-        }
-
-        // setIsActive(bgActive);
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     
-        // 👇️ or set to true
-        // setIsActive(true);
-        // bg='hsl(216, 12%, 54%)'
-      };
+    const data = {
+      index: isActive //e.target.index.value,
+    }
 
+    const JSONdata = JSON.stringify(data)
 
-    return (
-        <>
-            <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                <link href="https://fonts.googleapis.com/css2?family=Overpass:wght@400;700&display=swap" rel="stylesheet" />
-            </Head>
-            <Flex align="center" justify="center" bg='hsl(216, 12%, 8%)' w='375px' h='100vh' color='white' fontFamily='Overpass'>
-                <Flex gap='10px' p='20px' align="start" justify="center" w='80%' bg='hsl(213, 19%, 18%)' h='50%' borderRadius='10' direction='column'>
-                    <Flex align="center" justify="center" borderRadius='50%' bg='hsl(216, 12%, 54%)' w='40px' h='40px' >
-                        <img src="icon-star.svg" alt='star' />
-                    </Flex>
-                    <Flex>
-                        <Text fontSize={24} fontWeight='700'>
-                            How did we do?
-                        </Text>
-                    </Flex>
-                    <Flex>
-                        <Text fontSize={15} fontWeight='400'>
-                            Please let us know how we did with your support request. All feedback is appreciated
-                            to help us improve our offering!
-                        </Text>
-                    </Flex>
-                    <Flex align="center" justify="space-between" w='100%'>
-                        <Button onClick={handleClick} display="flex" alignItems="center" justifyContent="center" borderRadius='50%' bg={isActive} w='40px' h='40px'>
-                            1
-                        </Button>
-                        <Button display="flex" alignItems="center" justifyContent="center" borderRadius='50%' bg='hsl(216, 12%, 54%)' w='40px' h='40px'>
-                            2
-                        </Button>
-                        <Button display="flex" alignItems="center" justifyContent="center" borderRadius='50%' bg='hsl(216, 12%, 54%)' w='40px' h='40px'>
-                            3
-                        </Button>
-                        <Button display="flex" alignItems="center" justifyContent="center" borderRadius='50%' bg='hsl(216, 12%, 54%)' w='40px' h='40px'>
-                            4
-                        </Button>
-                        <Button display="flex" alignItems="center" justifyContent="center" borderRadius='50%' bg='hsl(216, 12%, 54%)' w='40px' h='40px'>
-                            5
-                        </Button>
-                    </Flex>
-                    <Flex align='center' justify='center' w='100%'>
-                        <Button w='100%' borderRadius='30px' bgColor='hsl(25, 97%, 53%)'>
-                            SUBMIT
-                        </Button>
-                    </Flex>
-                </Flex>
+    const endpoint = '/api/form'
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSONdata,
+    }
+
+    const response = await fetch(endpoint, options)
+
+    const result = await response.json()
+    alert(`Is this your full name: ${result.data}`)
+
+  };
+
+  return (
+    <>
+      <Head></Head>
+      <Flex
+        align="center"
+        justify="center"
+        bg="hsl(228, 11%, 9%)"
+        w="375px"
+        h="100vh"
+        color="white"
+        fontFamily="Overpass"
+      >
+        <Flex
+          gap="10px"
+          p="20px"
+          align="start"
+          justify="center"
+          w="80%"
+          bg="hsl(215, 20%, 17%)"
+          // h="50%"
+          borderRadius="10"
+          direction="column"
+        >
+          <Flex
+            align="center"
+            justify="center"
+            borderRadius="50%"
+            bg="hsl(210, 19%, 19%)"
+            w="40px"
+            h="40px"
+          >
+            <img src="icon-star.svg" alt="star" />
+          </Flex>
+          <Flex>
+            <Text fontSize={24} fontWeight="700">
+              How did we do?
+            </Text>
+          </Flex>
+          <Flex>
+            <Text fontSize={15} fontWeight="400">
+              Please let us know how we did with your support request. All
+              feedback is appreciated to help us improve our offering!
+            </Text>
+          </Flex>
+          <Flex
+            align="center"
+            justify="center"
+            w="100%"
+            direction="column"
+            gap="10px"
+          >
+            <Flex
+              as="form"
+              onSubmit={handleSubmit}
+              align="center"
+              justify="space-between"
+              w="100%"
+              gap="10px"
+              direction="column"
+            >
+              <Flex
+                align="center"
+                justify="space-between"
+                w="100%"
+                gap="10px"
+                direction="row"
+              >
+                {feedbackScore.map((item, index) => (
+                  <Button
+                    as="Radio"
+                    value={item}
+                    id={item}
+                    name={item}
+                    required
+                    onClick={() => chooseFeedback(index)}
+                    bg={
+                      isActive === index
+                        ? "hsl(214, 12%, 54%)"
+                        : "hsl(210, 19%, 24%)"
+                    }
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    borderRadius="50%"
+                    w="40px"
+                    h="40px"
+                    key={index}
+                    _hover={{ bg: "hsl(25, 97%, 53%)" }}
+                    cursor="pointer"
+                  >
+                    {item}
+                  </Button>
+                ))}
+              </Flex>
+              <Flex align="center" justify="center" w="100%">
+                <Button
+                  type="submit"
+                  w="100%"
+                  borderRadius="30px"
+                  bgColor="hsl(25, 97%, 53%)"
+                  _hover={{
+                    bg: "hsl(0, 0%, 100%)",
+                    color: "hsl(25, 97%, 53%)",
+                  }}
+                >
+                  SUBMIT
+                </Button>
+              </Flex>
             </Flex>
-        </>
-    )
+          </Flex>
+        </Flex>
+      </Flex>
+    </>
+  );
 }
 
-export default About
+export default About;
